b0e385e4325c72360bb446b9fff077035c22b3ef
Mironov <MMironov@1cbit.ru>
2018-10-04 16:04:52 +0300

    tcp echo server

---
+++ network_protocols/tcp_echo_server.py
@@ -0,0 +1,19 @@
+import socket
+
+
+def listen():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', 2222))
+    s.listen(5)
+
+    while True:
+        conn, adrr = s.accept()
+        while True:
+            data = conn.recv(1024)
+            if not data or data == b'close':
+                break
+            conn.close()
+
+
+if __name__ == '__main__':
+    listen()
