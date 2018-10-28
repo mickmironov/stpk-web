29a4c86cc8a6f46ba506d88b6dca89525b2f2704
Mikhail Mironov <xmikezx@gmail.com>
2018-10-28 14:59:19 +0300

    wsgi 5th attempt

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo pip3 install gunicorn==17.5
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
