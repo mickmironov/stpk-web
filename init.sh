db391557d006ad41399fd726837cd489c7d6ba5b
Mikhail Mironov <xmikezx@gmail.com>
2018-10-31 00:19:05 +0300

    django setup 6th attempt

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 sudo pip3 install gunicorn==17.5
-#sudo pip3 install --upgrade django
+sudo pip3 install --upgrade django
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
