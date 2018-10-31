31fcc4a94827ba469f6912bdd942714b2e316c53
Mikhail Mironov <xmikezx@gmail.com>
2018-10-31 22:39:13 +0300

    django setup 15th attempt

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-sudo rpl usr/bin/python usr/bin/python3 /usr/sbin/gunicorn-debian
-sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn
-sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn_django
-sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn_django
-sudo rpl 17.5 19.9.0 /usr/sbin/gunicorn-debian
-sudo rpl 17.5 19.9.0 /usr/bin/gunicorn
-sudo rpl 17.5 19.9.0 /usr/bin/gunicorn_django
-sudo rpl 17.5 19.9.0 /usr/bin/gunicorn_django
+sudo sed -i -e 's/17.5/19.9.0/g' /usr/sbin/gunicorn-debian
+sudo sed -i -e 's/17.5/19.9.0/g' /usr/bin/gunicorn
+sudo sed -i -e 's/17.5/19.9.0/g' /usr/bin/gunicorn_django
+sudo sed -i -e 's/17.5/19.9.0/g' /usr/bin/gunicorn_paster
+sudo sed -i -e 's/python/python3/g' /usr/sbin/gunicorn-debian
+sudo sed -i -e 's/python/python3/g' /usr/bin/gunicorn
+sudo sed -i -e 's/python/python3/g' /usr/bin/gunicorn_django
+sudo sed -i -e 's/python/python3/g' /usr/bin/gunicorn_paster
 sudo pip3 install django==2.0.9
 sudo pip3 install gunicorn --upgrade
 sudo unlink /etc/nginx/sites-enabled/default
