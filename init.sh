d3826e6ab21621add34c5c72b57f83e68fa42944
Mikhail Mironov <xmikezx@gmail.com>
2018-10-31 22:29:22 +0300

    django setup 14th attempt

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
+sudo rpl usr/bin/python usr/bin/python3 /usr/sbin/gunicorn-debian
+sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn
+sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn_django
+sudo rpl usr/bin/python usr/bin/python3 /usr/bin/gunicorn_django
+sudo rpl 17.5 19.9.0 /usr/sbin/gunicorn-debian
+sudo rpl 17.5 19.9.0 /usr/bin/gunicorn
+sudo rpl 17.5 19.9.0 /usr/bin/gunicorn_django
+sudo rpl 17.5 19.9.0 /usr/bin/gunicorn_django
 sudo pip3 install django==2.0.9
 sudo pip3 install gunicorn --upgrade
 sudo unlink /etc/nginx/sites-enabled/default
