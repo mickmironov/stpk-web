c084e4541c8eb2ef52fc6b7a02c21d395f959289
Mikhail Mironov <xmikezx@gmail.com>
2019-02-08 22:02:02 +0300

    models Question and Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(default='', max_length=1024)
+    text = models.TextField(default='')
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+    def __str__(self):
+        return self.title
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stpk_web;"
+mysql -uroot -e "grant all privileges on stpk_web.* to 'box'@'localhost' with grant option;"
+python manage.py makemigrations qa
+python manage.py migrate qa
